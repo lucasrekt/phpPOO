62eecb83d50d14cd58cc1aebf2d3f36dd698b801
Lucas Gabriel de Moura Santos <116088167+lucasrekt@users.noreply.github.com>
2023-06-05 17:15:15 -0300

    Add files via upload

--- UltraEmojiCombat/index.php
+++ UltraEmojiCombat/index.php
@@ -11,6 +11,7 @@
 
     <?php 
         require_once 'Lutador.php';
+        require_once 'Luta.php';
 
         $l = array();
         $l[0] = new Lutador("Pretty Boy", "França", 30, 1.75, 90.9, 11, 2, 1);
@@ -20,9 +21,6 @@
         $l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
         $l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81, 105.7, 12, 2, 4);
 
-        $l[3]->perderLuta();
-        $l[3]->apresentar();
-        $l[3]->status();
     ?>
     
     </pre>
